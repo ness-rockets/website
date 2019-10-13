e2ba4270b390dfe77a8d9c4652e533048a05811e
Yan <yannunes@gmail.com>
2019-10-13 14:51:41 +0200

    Surgery in progress

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# website
+Ness Rocket Website
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./nessrockets.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <img src={logo} alt="logo" />
+    </div>
+  );
+}
+
+export default App;
